1d3c9228ab6d124745b46dd7b106f080f6bc4512
Animesh-Ghosh <animesh00914902017@msi-ggsip.org>
2021-11-28 22:03:06 +0530

    Handle Connection Request modal
    Automated closing the Connection Request modal.

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@
 let isAutoConnecting = false
 let intervalId
 
+const connectionRequestsCountSpan = document.getElementById('connection-count')
 const autoConnectBtn = document.getElementById('auto-connect')
 autoConnectBtn.onclick = async function () {
   isAutoConnecting = ! isAutoConnecting
@@ -19,6 +20,8 @@ autoConnectBtn.onclick = async function () {
         function: autoConnect,
         args: [isAutoConnecting]
       })
+      const connectionRequestsCount = Number(connectionRequestsCountSpan.textContent)
+      connectionRequestsCountSpan.textContent = connectionRequestsCount + 1
     }, 5000)
   }
   if (intervalId && ! isAutoConnecting) {
@@ -26,21 +29,39 @@ autoConnectBtn.onclick = async function () {
   }
 }
 
-function autoConnect(isAutoConnecting) {
+async function autoConnect(isAutoConnecting) {
+  // since we cannot pass args to the injected files,
+  // we make do with closures to simplify the injected code
+  const isConnectable = people => {
+    return people
+      .querySelector('.entity-result__actions button.artdeco-button .artdeco-button__text')
+      .textContent
+      .trim() == 'Connect'
+  }
+  const awaitModal = async function (ms) {
+    return new Promise((resolve) => {
+      window.setTimeout(function () {
+        resolve(document.querySelector('#artdeco-modal-outlet .artdeco-modal-overlay'))
+      }, ms)
+    })
+  }
+
   // not the best way to query the results on the page
   // but hopefully it's not too costly
   const peoples = document.getElementsByClassName('entity-result__item')
   for (const people of peoples) {
-    // TODO: refactor below conditional into a function
-    if (people.querySelector('.entity-result__actions button.artdeco-button .artdeco-button__text').textContent.trim() == 'Connect') {
-      console.log('Clicking', people.querySelector('.entity-result__actions button.artdeco-button'))
+    if (isConnectable(people)) {
+      // console.log('Clicking', people.querySelector('.entity-result__actions button.artdeco-button'))
       people.querySelector('.entity-result__actions button.artdeco-button').click()
 
-      // TODO: handle modal
-      /*
-      console.log('Closing modal...')
-      document.querySelector('#artdeco-modal-outlet .artdeco-modal-overlay').click()
-      */
+      // console.log('Closing modal...')
+      /**
+       * we wait for some time for the modal to render on the DOM
+       * and then click on the Send now button
+       * removing the modal from the DOM was something that I could not figure out
+       */
+      const modal = await awaitModal(500)
+      modal.querySelector('button[aria-label="Send now"].artdeco-button').click()
 
       // return so that on the next run, the first of Connectable entities
       // are sent a Connection request
